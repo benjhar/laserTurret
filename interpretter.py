0028d2eeb1f21710478b162e6604271f87529bc2
thenamesweretakenalready <leet_haker@cyber-wizard.com>
2020-06-04 13:05:16 +0100

    fix?

--- interpretter.py
+++ interpretter.py
@@ -11,15 +11,15 @@ with open('movement.json') as f:
     # print(movement)
 
 if direction == "l":
-    ser.write(bytes("servo1 {-amount}".format(-amount), encoding='UTF-8'))
+    ser.write(bytes("servo1 {}".format(amount*-1), encoding='UTF-8'))
 
 if direction == "r":
-    ser.write(bytes("servo1 {amount}".format(amount), encoding='UTF-8'))
+    ser.write(bytes("servo1 {}".format(amount), encoding='UTF-8'))
 
 if direction == "u":
     ser.write(bytes("servo2 {}".format(amount), encoding='UTF-8'))
 
 if direction == "d":
-    ser.write(bytes("servo2 {}".format(-amount), encoding='UTF-8'))
+    ser.write(bytes("servo2 {}".format(amount*-1), encoding='UTF-8'))
 
 ser.close()
